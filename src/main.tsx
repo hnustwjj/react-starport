e0ccf68ff1b479007de908344d94a7ec1c5edf0d
hnustwjj <948065841@qq.com>
2022-06-14 22:44:47 +0800

    todo

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import 'virtual:windi.css'
 import App from './App'
+//TODO: 根据proxyEl获取到FloatContainer应该所在的位置
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <div className='w-[100vw] h-[100vh] bg-black text-white'>
