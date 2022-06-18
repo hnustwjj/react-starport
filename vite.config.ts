46c48f027e1b25e6cc4a181a00621bf9361b6481
hnustwjj <948065841@qq.com>
2022-06-18 22:18:57 +0800

    fix: memo custom equal

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [WindiCSS(), react()],
-  server: {
-    host: '0.0.0.0',
-  },
 })
