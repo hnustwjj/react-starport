900a3ad5831bdb7a8935c90d2c5165c5107a1972
hnustwjj <948065841@qq.com>
2022-07-02 19:21:42 +0800

    update: 调整demo样式

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,7 @@ const Home = memo(() => {
             enlarge
           </button>
         </nav>
-        <div className='mt-10 flex <md:(flex-col items-center) md:(justify-center w-[60%] items-center)'>
+        <div className='mt-10 flex <md:(flex-col-reverse items-center) md:(justify-center w-[60%] items-center)'>
           <p flex='1' className='m-10'>
             现代多使用于魔兽世界中，戏称死了以后尸体躺在地上长期不消失的现象。而游戏职业术士作为最容易集火导致死亡的目标，被玩家戏称为“躺尸术”。
             《魔兽世界》中有一位术士ID为爱你就这样，其于游戏版本5.4初期为了去金团消费而买空淘宝金币，因此其得以红极一时。引申意：在集体宿舍中被人吵得睡不着觉，在床上干躺着，也称为躺尸，可见宿舍之吵环境之差。
@@ -49,7 +49,7 @@ const Home = memo(() => {
             其它引申意：形容睡觉睡的死，一动不动的，不管外边有什么情况也不会有什么反应了！
           </p>
         </div>
-        <div className='mt-10 flex <md:(flex-col items-center) md:(justify-center w-[60%] items-center)'>
+        <div className='mt-10 flex <md:(flex-col-reverse items-center) md:(justify-center w-[60%] items-center)'>
           <p flex='1' className='m-10'>
             现代多使用于魔兽世界中，戏称死了以后尸体躺在地上长期不消失的现象。而游戏职业术士作为最容易集火导致死亡的目标，被玩家戏称为“躺尸术”。
             《魔兽世界》中有一位术士ID为爱你就这样，其于游戏版本5.4初期为了去金团消费而买空淘宝金币，因此其得以红极一时。引申意：在集体宿舍中被人吵得睡不着觉，在床上干躺着，也称为躺尸，可见宿舍之吵环境之差。
@@ -75,7 +75,7 @@ const Home = memo(() => {
             其它引申意：形容睡觉睡的死，一动不动的，不管外边有什么情况也不会有什么反应了！
           </p>
         </div>
-        <div className='mt-10 flex <md:(flex-col items-center) md:(justify-center w-[60%] items-center)'>
+        <div className='mt-10 flex <md:(flex-col-reverse items-center) md:(justify-center w-[60%] items-center)'>
           <p flex='1' className='m-10'>
             现代多使用于魔兽世界中，戏称死了以后尸体躺在地上长期不消失的现象。而游戏职业术士作为最容易集火导致死亡的目标，被玩家戏称为“躺尸术”。
             《魔兽世界》中有一位术士ID为爱你就这样，其于游戏版本5.4初期为了去金团消费而买空淘宝金币，因此其得以红极一时。引申意：在集体宿舍中被人吵得睡不着觉，在床上干躺着，也称为躺尸，可见宿舍之吵环境之差。
