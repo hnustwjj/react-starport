5b52b16a434e5f63e037ce41422f7a77c0ea24f7
hnustwjj <948065841@qq.com>
2022-07-03 17:46:20 +0800

    fix: hash模式

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import 'virtual:windi.css'
 import App from './App'
 //TODO: 根据proxyEl获取到FloatContainer应该所在的位置
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
