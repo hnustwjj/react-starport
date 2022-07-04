a72675940f284075090e7c8738b8a058f256b46f
hnustwjj <948065841@qq.com>
2022-07-04 09:57:17 +0800

    fix: error

--- src/components/TheNav.tsx
+++ src/components/TheNav.tsx
@@ -3,29 +3,29 @@ import React from 'react'
 export default function TheNav() {
   return (
     <nav
-      px="8" py="4" mb="4"
-      border="b gray-400/10"
-      flex="~ gap-2"
-      bg="white"
-      text="xl [#374751]" items="center"
+      className='px-8 py-4 mb-4'
+      border='b gray-400/10'
+      flex='~ gap-2'
+      bg='white'
+      text='xl [#374751]'
+      items='center'
     >
-      <Link to="/" flex="~ gap-2" items-center>
+      <Link to='/' flex='~ gap-2' items-center>
         {/* <img src="/favicon.svg" h-10 /> */}
-        <div className="font-mono text-center  mr-6 text-md">
+        <div className='font-mono text-center  mr-6 text-md'>
           React Starport
         </div>
       </Link>
 
-      <div className="flex-auto" />
+      <div className='flex-auto' />
 
-      <div flex="~ gap-4" items="center">
+      <div flex='~ gap-4' items='center'>
         <a
-          className="icon-btn i-carbon-logo-github"
-          
-          rel="noreferrer"
-          href="https://github.com/hnustwjj/react-starport"
-          target="_blank"
-          title="GitHub"
+          className='icon-btn i-carbon-logo-github'
+          rel='noreferrer'
+          href='https://github.com/hnustwjj/react-starport'
+          target='_blank'
+          title='GitHub'
         />
       </div>
     </nav>
